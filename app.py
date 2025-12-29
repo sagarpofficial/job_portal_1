6db43349d8a91d8d8cf5a708121036f58f560001
Sagar P <sagarpsagarp123@gmail.com>
2025-12-29 18:38:15 +0530

    Fix mail config for Render

--- app.py
+++ app.py
@@ -60,16 +60,17 @@ admin_role_permissions = db.Table(
 )
 
 # -------------------- MAIL --------------------
-from dotenv import load_dotenv
-load_dotenv()
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
 
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
+
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT", 587))
+app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS", "true").lower() == "true"
+app.config['MAIL_USE_SSL'] = False
+
+app.config['MAIL_USERNAME'] = os.getenv("jobsour997@gmail.com")
+app.config['MAIL_PASSWORD'] = os.getenv("iiprvglsjgieiiny")
+app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
 
 mail = Mail(app)
 # -------------------- UPLOADS --------------------
